e2a86294bac880f7f4377513a138bb33c3eb4284
Lance Nines <lance.nines@gmail.com>
2026-03-21 16:03:43 -0400

    Add Discogs metadata columns to vinyl schema

--- schema.sql
+++ schema.sql
@@ -1,4 +1,22 @@
 PRAGMA foreign_keys = ON;
+/*
+func searchForImage(albumTitle, artist string) (discogsResp, error) {
+	albumTitle = strings.TrimSpace(albumTitle)
+	artist = strings.TrimSpace(artist)
+	if albumTitle == "" || artist == "" {
+		return discogsResp{}, fmt.Errorf("must provide strings for both albumTitle and artist")
+	}
+	// fmt resp for all three endpoints
+	//https://api.discogs.com/database/search?release_title=a%20collection&artist=third%20eye%20blind&format=vinyl&per_page=1&page=1
+	//"results" "master_id"
+	//https://api.discogs.com/masters/594810
+	//"uri" - discogs direct url
+	//"genres" - array of styles by string
+	//"styles" - array of styles by string
+	//"images" [] - the entry with "type" : "primary", "uri" : "https://i.discogs.com/oauwCBqdKyEn_31qZQon5zHrKLSs4k8BDmUvEptK2SI/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTMxMTcw/ODYtMTU0MjY3Njk1/OC03NDk2LmpwZWc.jpeg"
+	return discogsResp{}, nil
+}
+*/
 
 CREATE TABLE IF NOT EXISTS vinyl_unique(
 	vinyl_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -6,6 +24,9 @@ CREATE TABLE IF NOT EXISTS vinyl_unique(
 	vinyl_artist TEXT NOT NULL,
 	vinyl_pressing_year INTEGER NOT NULL,
 	first_pressing INTEGER NOT NULL, -- 0 false 1 true
+	discogs_master_id INTEGER,
+	styles TEXT, -- comma separated list of styles "Alternative Rock,Punk" etc
+	genres TEXT, -- comma separated list of genres "Rock,Heavy Metal" etc
 	image_extension TEXT NOT NULL,
 	cover_raw_blob BLOB NOT NULL,
 	cover_embedding BLOB NOT NULL,
